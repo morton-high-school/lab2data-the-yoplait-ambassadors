764564d773a719ab75820f5ccb695f8606b58541
jamesjulich <51384945+jamesjulich@users.noreply.github.com>
2019-12-10 14:56:02 -0600

    Moved files, created classes.

---
+++ LinearRegression.java
@@ -0,0 +1,20 @@
+public class LinearRegression
+{
+    ArrayList<YearData> data;
+
+    double slope;
+    double yIntercept;
+
+    public LinearRegression(ArrayList<YearData> data)
+    {
+        this.data = data;
+
+        double[]
+    }
+
+    //x axis = carbon, y axis = temp,
+    public double[] findTemperatureCarbonRegression()
+    {
+
+    }
+}
---
+++ YearData.java
@@ -0,0 +1,13 @@
+public class YearData
+{
+    public int year;
+    public double averageTemp;
+    public double carbonEmissions;
+
+    public YearData(int year, double averageTemp, double carbonEmissions)
+    {
+        this.year = year;
+        this.averageTemp = averageTemp;
+        this.carbonEmissions = carbonEmissions;
+    }
+}
